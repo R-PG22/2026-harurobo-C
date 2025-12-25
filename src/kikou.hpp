b46cf43b85ffa2fdfe67eca7578100c658ab9e03
R-PG22 <tsukisigure1225@gmail.com>
2025-12-25 10:45:53 +0900

    mainを大幅に変え、機構を操作できるようにした

---
+++ src/kikou.hpp
@@ -0,0 +1,119 @@
+#ifndef KIKOU_HPP
+#define KIKOU_HPP
+
+#include "mbed.h"
+#include <iostream>
+#include "controler.hpp"
+CAN can1(PB_12, PB_13, 1e6);
+CANMessage msg;
+// CANMessage msg1;
+// リミットスイッチ読み取り関数
+void read_limit(bool (&is_sw_push)[5])
+{
+    if (can1.read(msg); msg.id == 9)
+    {
+        uint8_t sw = msg.data[5]; // スイッチの値の内容
+        for (int i = 0; i < 5; i++)
+        {
+            is_sw_push[i] = (sw >> i) & 0x01;
+            // シフトで判別する位を最下位ビットへ移動し、1と論理積を行うことで各位の値を判別する。
+        }
+    }
+}
+
+// エンコーダー読み取り関数
+void read_encoder(int16_t (&enc)[5])
+{
+    if (can1.read(msg); msg.id == 9)
+    {
+        enc[0] = msg.data[7] << 8 | msg.data[6];
+    }
+    if (can1.read(msg); msg.id == 10)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            enc[i + 1] = msg.data[(i * 2) + 1] << 8 | msg.data[i * 2];
+        }
+    }
+}
+
+// PWM計算関数
+void pwm_calculation(int16_t(&pwm_value), bool button1, bool button2, int value)
+{
+    pwm_value = (button1 - button2) * value;
+}
+
+void sensor_processing(int16_t(&pwm_value), bool limit, bool up_down)
+{
+    if (limit && ((pwm_value > 0) == up_down))
+    {
+        pwm_value = 0;
+    }
+}
+void mekanamu(int16_t (&pwm)[4])
+{
+    // メカナム用の足回り計算
+    // printf("%d\n",(int)stick_value["lx"]);
+    float omuni_value = 15000;
+    float power = hypot(stick_value["lx"], stick_value["ly"]);
+    float angle = atan2((-1 * stick_value["ly"]), stick_value["lx"]);
+    int num2 = (int)(6000.0 * stick_value["rx"]);
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        int num = -1 * (int)(sin((M_PI / 180 * (90 * i + 45)) + angle) * power * omuni_value);
+        if (num != 0)
+        {
+            pwm[i] = num;
+        }
+        else
+        {
+            pwm[i] = num2;
+        }
+
+        if (pwm[i] > 15000)
+        {
+            pwm[i] = 0;
+        }
+        else if (pwm[i] < -15000)
+        {
+            pwm[i] = 0;
+        }
+    }
+}
+// センサー処理関数
+// limit: リミットスイッチの状態
+// up_down: 正負の判定(true:正の場合停止)
+// void pid_tick()
+// {
+//     // pid_control のシグネチャに合わせて呼ぶ（グローバル変数を参照）
+//     pid_control(pwm1[2], encoder_value[0], want_k);
+// }
+float i_control = 0;
+int before_error = 0;
+float dt = 0.04; // 制御周期
+float p_gain = 1.0;
+float i_gain = 0.0;
+float d_gain = 0.0;
+void pid_control(int16_t(&pwm_value), int16_t encoder_value, int16_t want_value)
+{
+    int error = want_value - encoder_value;
+    float p_control = error;
+    i_control += error * dt;
+    float d_control = (error - before_error) / dt;
+
+    pwm_value = p_gain * p_control + i_gain * i_control + d_gain * d_control;
+    if (pwm_value > 10000)
+    {
+        pwm_value = 10000;
+    }
+    else if (pwm_value < -10000)
+    {
+        pwm_value = -10000;
+    }
+    before_error = error;
+    // printf("%d\n", pwm_value);
+}
+
+#endif // KIKOU_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,48 @@
-#include <map>
-#include <array>
+#include <iostream>
+#include <cmath>
 #include "mbed.h"
 #include "FirstPenguin.hpp"
+#include "kikou.hpp"
+#include "controler.hpp"
 
 // 通信系の設定
 CAN can(PB_12, PB_13, 1e6);
-CANMessage msg;
+CANMessage msg1;
 constexpr int can_id = 35;
 constexpr int penguin_number = 0;
 FirstPenguin penguin(can_id, can);
 // ロリコンは0が横、1が縦とする
 
-BufferedSerial pc(USBTX, USBRX, 115200);
-char c;
-
 
 // 出力関連の設定
-array<float, 4> wheel_pwm = {0};
-constexpr int MAX_PWM = 15000;
-float per_adjust = 0.1; // pwmをゆっくり上げるための係数 0.1f ~ 1.0f
-
-enum class State{
-    FORWARD,
-    BACK,
-    RIGHT,
-    LEFT,
-    STOP
-};
-map<char, State> char_to_state = {
-    {'w', State::FORWARD},
-    {'s', State::BACK},
-    {'a', State::LEFT},
-    {'d', State::RIGHT},
-    {'x', State::STOP}
-};
-map<State, std::array<int, 4>> state_to_pwm = {
-    {State::FORWARD, {1, 1, -1, -1}},
-    {State::BACK,    {-1, -1, 1, 1}},
-    {State::RIGHT,   {-1, 1, 1, -1}},
-    {State::LEFT,    {1, -1, -1, 1}},
-    {State::STOP,    {0, 0, 0, 0}}
-};
-
-State state;
-
-int per_encoder = 1024;
-int prev_encoder[4] = {0};
-
-void read_controller();
-void pwm_correction(int tolerance_enc);
-
-auto clamp = [](auto val, auto min_val, auto max_val){return std::max(std::min(val, max_val), min_val);};
+int16_t pwm1[4] = {0};
+bool limit_value[5] = {0};
+int16_t encoder_value[5] = {0};
+// constexpr int MAX_PWM = 15000;
+// float per_adjust = 0.1; // pwmをゆっくり上げるための係数 0.1f ~ 1.0f
 
 int main(){
-    auto state = State::STOP;
-    auto prev_state = State::STOP;
-
     while(true){
-        penguin.receive[penguin_number].set(msg.data);
         read_controller();
-        if (prev_state == state){
-            per_adjust *= 1.5f;
-            clamp(per_adjust, 0.1f, 1.0f);
-        }else{
-            per_adjust = 0.1f;
-        }
-        wheel_pwm = pwm_correction(15, 0.1f);
+        read_limit(limit_value);
+        read_encoder(encoder_value);
 
-        for (int i = 0; i < 4; ++i){
-            penguin.pwm[i] = static_cast<int>(wheel_pwm[i] * per_adjust * MAX_PWM);
-            clamp(int(penguin.pwm[i]), -MAX_PWM, MAX_PWM);
-        }
+        // コントローラー入力からpwm計算
+        pwm_calculation(pwm1[0], controller["L1"], controller["R1"], 15000);  // カード1,l2,r2,pwm_data配列0番
+        pwm_calculation(pwm1[1], controller["ci"], controller["sq"], 5000);   // カード2,l,r,pwm_data配列1番
+        pwm_calculation(pwm1[2], controller["tri"], controller["cr"], 15000); // カード3,circle,cross,pwm_data配列2番
+
+        // センサー処理
+        // sensor_processing(pwm1[0], limit_value[0], 1); // ロジャー下リミット
+        // sensor_processing(pwm1[0], limit_value[1], 0); // ロジャー上リミット
+        // sensor_processing(pwm1[1], limit_value[2], 1); // garaggaraリミット
+
+        // メカナム計算
+        mekanamu(penguin.pwm);
+
+        // モタドラへcan送信
         penguin.send();
-        prev_state = state;
+        CANMessage msg1(2, (const uint8_t *)pwm1, 8);
+        can.write(msg1);
     }
-}
-
-void read_controller(){
-    int len = pc.read(&c, 1);
-    if (len > 0){
-        if (c == 'w'){
-            state = State::FORWARD;
-        }else if (c == 's'){
-            state = State::BACK;
-        }else if (c == 'a'){
-            state = State::LEFT;
-        }else if (c == 'd'){
-            state = State::RIGHT;
-        }else if (c == 'x'){
-            state = State::STOP;
-        }
-    }
-    for (int i = 0; i < 4; ++i){
-        wheel_pwm[i] = state_to_pwm[state][i];
-    }
-}
-
-array<float, 4> pwm_correction(int tolerance_enc, float p_gain){
-    array<float, 4> correction_pwm = {penguin.pwm[0], penguin.pwm[1], penguin.pwm[2], penguin.pwm[3]};
-    int diff_01 = penguin.receive[0].enc - penguin.receive[1].enc;
-    int diff_12 = penguin.receive[1].enc - penguin.receive[2].enc;
-    int diff_23 = penguin.receive[2].enc - penguin.receive[3].enc;
-    int diff_30 = penguin.receive[3].enc - penguin.receive[0].enc;
-    switch(state){
-        case State::FORWARD:
-        case State::BACK:
-            if (abs(penguin.receive[0].enc) > tolerance_enc){
-                if (abs(diff_01) > tolerance_enc){
-                    correction_pwm[0] -= diff_01 * p_gain;
-                    correction_pwm[1] += diff_01 * p_gain;
-                }
-                if (abs(diff_23) > tolerance_enc){
-                    correction_pwm[2] -= diff_23 * p_gain;
-                    correction_pwm[3] += diff_23 * p_gain;
-                }
-                int diff_01_23 = (penguin.receive[0].enc + penguin.receive[1].enc) / 2 - 
-                                (penguin.receive[2].enc + penguin.receive[3].enc) / 2;
-                if (abs(diff_01_23) > tolerance_enc){
-                    correction_pwm[0] -= diff_01_23 * p_gain;
-                    correction_pwm[1] -= diff_01_23 * p_gain;
-                    correction_pwm[2] += diff_01_23 * p_gain;
-                    correction_pwm[3] += diff_01_23 * p_gain;
-                }
-            }
-            break;
-        case State::RIGHT:
-        case State::LEFT:
-            if (abs(penguin.receive[1].enc) > tolerance_enc){
-                if (abs(diff_12) > tolerance_enc){
-                    correction_pwm[1] -= diff_12 * p_gain;
-                    correction_pwm[2] += diff_12 * p_gain;
-                }
-                if (abs(diff_30) > tolerance_enc){
-                    correction_pwm[2] -= diff_30 * p_gain;
-                    correction_pwm[3] += diff_30 * p_gain;
-                }
-                int diff_12_30 = (penguin.receive[1].enc + penguin.receive[2].enc) / 2 - 
-                                (penguin.receive[3].enc + penguin.receive[0].enc) / 2;
-                if (abs(diff_12_30) > tolerance_enc){
-                    correction_pwm[0] -= diff_12_30 * p_gain;
-                    correction_pwm[1] -= diff_12_30 * p_gain;
-                    correction_pwm[2] += diff_12_30 * p_gain;
-                    correction_pwm[3] += diff_12_30 * p_gain;
-                }
-            }
-            break;
-        case State::STOP:
-            for (int i = 0; i < 4; ++i){
-                penguin.receive[i].enc = 0;
-            }
-            break;
-        default:
-            break;
-    }
-    return correction_pwm;
 }
